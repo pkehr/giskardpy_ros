fab29c672e591aa612cd04b55937b0ffdeaecab9
pkehr <philk99@web.de>
2025-05-20 14:30:29 +0200

    [turtle_config] add turtle as seperate group now

--- src/giskardpy_ros/configs/iai_robots/hsr.py
+++ src/giskardpy_ros/configs/iai_robots/hsr.py
@@ -158,8 +158,10 @@ class SuturoArenaWithHSRWithTurtleBotConfig(WorldWithHSRConfig):
 
         link_name = PrefixName('base_footprint_turtle', 'turtle')
         self.add_empty_link(link_name)
-        #self.world.links[link_name].collisions.append(CylinderGeometry(height=0.62, radius=0.25, color=ColorRGBA(1,1,1,1)))
+        self.world.links[link_name].collisions.append(CylinderGeometry(height=0.01, radius=0.25, color=ColorRGBA(1,1,1,1)))
         self.add_6dof_joint(parent_link=self.map_name, child_link=link_name, joint_name=PrefixName('turtle_joint', 'turtle'))
+
+        self.world.register_group('turtle', root_link_name=link_name)
 
 class HSRCollisionAvoidanceConfig(CollisionAvoidanceConfig):
     def __init__(self, drive_joint_name: str = 'brumbrum'):
